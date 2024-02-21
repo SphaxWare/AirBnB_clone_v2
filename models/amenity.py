b57229fac2b6e3cbb47c2e65b6990ced8b118290
Sphax <itssphax123@gmail.com>
2024-02-21 12:56:21 -0800

    make user

--- models/amenity.py
+++ models/amenity.py
@@ -10,6 +10,5 @@ from models.base_model import BaseModel, Base
 class Amenity(BaseModel, Base):
     """Represents an amenity data set."""
     __tablename__ = 'amenities'
-    name = Column(
-            String(128), nullable=False
+    name = Column(String(128), nullable=False
             ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
--- models/user.py
+++ models/user.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
-from models.place import Place
-from models.review import Review
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
 
@@ -15,13 +13,3 @@ class User(BaseModel):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-    places = relationship(
-        'Place',
-        cascade="all, delete, delete-orphan",
-        backref='user'
-    )
-    reviews = relationship(
-        'Review',
-        cascade="all, delete, delete-orphan",
-        backref='user'
-    )
