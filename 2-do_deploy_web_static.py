bcafe81f08c85033cbcdd64df89c306c5cc3312d
Sphax <itssphax123@gmail.com>
2024-03-06 15:52:55 -0800

    fix pycode

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -10,6 +10,7 @@ from os.path import exists
 env.hosts = ['100.26.241.117', '107.22.144.250']
 env.user = 'ubuntu'
 
+
 def do_deploy(archive_path):
     """
     Distributes an archive to web servers.
@@ -30,7 +31,8 @@ def do_deploy(archive_path):
 
         # Extract the archive to /data/web_static/releases/
         archive_filename = archive_path.split('/')[-1]
-        release_path = '/data/web_static/releases/{}'.format(archive_filename.split('.')[0])
+        release_path = '/data/web_static/releases/{}'.format(
+                              archive_filename.split('.')[0])
         run('mkdir -p {}'.format(release_path))
         run('tar -xzf /tmp/{} -C {}'.format(archive_filename, release_path))
 
