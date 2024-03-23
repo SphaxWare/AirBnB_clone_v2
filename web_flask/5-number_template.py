2c2775d6a16ecd0eb4e1c9152751af190f8dc933
Sphax <itssphax123@gmail.com>
2024-03-22 19:04:18 -0700

    fix

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -39,7 +39,7 @@ def number(n):
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
     if isinstance(n, int):
-        return render_template('number_template.html', number=n)
+        return render_template('5-number.html', number=n)
 
 
 if __name__ == '__main__':
