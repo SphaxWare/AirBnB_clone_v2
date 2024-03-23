178d65ebf2f1a39d8f4428e565822df75f372264
Sphax <itssphax123@gmail.com>
2024-03-22 19:02:01 -0700

    5. Number template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+Simple flask app
+"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_HBNB():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_is_fun(text):
+    text = text.replace('_', ' ')
+    return f"C {text}"
+
+
+@app.route("/python/", defaults={'text': 'is_cool'}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text):
+    text = text.replace('_', ' ')
+    return f"Python {text}"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    return f"{n} is a number"
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    if isinstance(n, int):
+        return render_template('number_template.html', number=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
